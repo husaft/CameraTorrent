a816f83c4504d7fae8ce3ff52621c15f3948bdd0
husaft <husaft@users.noreply.github.com>
2023-02-18 05:17:07 +0000

    deploy: eeaac375df65d2bb820a06a0e1aeb36ed5d894b6

---
+++ js/util.js
@@ -0,0 +1,81 @@
+
+window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
+
+window.getFrame = (src, dest, dotNetHelper) => {
+    const video = document.getElementById(src);
+    const w = video.videoWidth;
+    const h = video.videoHeight;
+
+    const canvas = document.getElementById(dest);
+    canvas.width = w;
+    canvas.height = h;
+
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, w, h);
+
+    const dataUrl = canvas.toDataURL("image/jpeg");
+    dotNetHelper.invokeMethodAsync('ProcessImage', dataUrl);
+}
+
+window.stopVideo = (src) => {
+    const video = document.getElementById(src);
+    if (video && "srcObject" in video) {
+        const stream = video.srcObject;
+        if (stream) {
+            const tracks = stream.getTracks();
+            tracks.forEach(track => track.stop());
+        }
+        video.srcObject = null;
+    }
+}
+
+window.startVideo = async (src) => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const setup = {
+            video: {
+                facingMode: { exact: "environment" },
+                width: { min: 1024, ideal: 1280, max: 1920 },
+                height: { min: 576, ideal: 720, max: 1080 }
+            }
+        };
+        let stream;
+        try {
+            stream = await navigator.mediaDevices.getUserMedia(setup);
+        } catch (err) {
+            setup.video.facingMode = null;
+            try {
+                stream = await navigator.mediaDevices.getUserMedia(setup);
+            } catch (sErr) {
+                const tmp = setup.video.width;
+                setup.video.width = setup.video.height;
+                setup.video.height = tmp;
+                stream = await navigator.mediaDevices.getUserMedia(setup);
+            }
+        }
+        const btn = document.getElementById(src + "Btn");
+        btn.style.display = "block";
+        const video = document.getElementById(src);
+        video.style.display = "block";
+
+        if ("srcObject" in video) {
+            video.srcObject = stream;
+        } else {
+            video.src = window.URL.createObjectURL(stream);
+        }
+        video.onloadedmetadata = function (_e) {
+            video.play();
+        };
+        video.style.webkitTransform = "scaleX(-1)";
+        video.style.transform = "scaleX(-1)";
+    }
+}
